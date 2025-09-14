fc568772093fd9091b09d66d6030ad3ffc55d27e
vijay18vp <voidprotocol18@gmail.com>
2025-09-14 13:12:20 +0530

    Learned bitWise operator

---
+++ LectureNotes/BitWise/bitWise.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int a = 4 ;
+    int b = 6 ;
+
+    cout << " a & b : " << (a & b) << endl ; 
+    cout << " a | b : " << (a | b ) << endl ; 
+    cout << "a ^ b : " << (a ^ b) << endl ;   
+    cout << "a << 2 : " << (a << 2) << endl ; 
+    return 0;
+}
