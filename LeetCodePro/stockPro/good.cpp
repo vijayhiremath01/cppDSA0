5d64e72fb50fdd91eaff6096393bbe6f5c586d4a
vijay18vp <voidprotocol18@gmail.com>
2025-10-16 01:13:28 +0530

    learning leetcode

--- LeetCodePro/stockPro/good.cpp
+++ LeetCodePro/stockPro/good.cpp
@@ -1,1 +1,24 @@
 // Good Code for better time complexity 
+
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+int main() {
+    vector<int> prices = {2 , 5, 7, 2, 4 , 8, 9 };
+    class Solution {
+        public : 
+            int maxProfit(vector<int> &prices){
+                int n = prices.size();
+                int buyAtPrice = INT_MAX ;
+                int maxProfit = 0 ; 
+                for(int i = 0 ; i < n ; i++){
+                    buyAtPrice = min(buyAtPrice , prices[i]);
+                    maxProfit = max(maxProfit , prices[i] - buyAtPrice);
+                }
+                return maxProfit ; 
+            }
+    };
+
+    cout << "The maximum profit will be : " << Solution().maxProfit(prices);
+}
