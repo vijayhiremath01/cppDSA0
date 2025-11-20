d4a0ad5e3309f12b610e526d324ad11c76bdc42b
vijay18vp <voidprotocol18@gmail.com>
2025-11-20 21:42:32 +0530

    env file created

--- LectureNotes/sorting/bubble.md
+++ LectureNotes/sorting/bubble.md
@@ -4,3 +4,21 @@ for example dry run
 4 1 5 2 3 
 first we compare 4 and 1 , 1 and 5 , 5 and 2 , 2 and 3 and for 4 and 1 we compare and psuh largest value to right side for ascending order ! 
 even though it is sorted after that also it runs upto n - 1 iteration times 
+time complexity for bubble sort is 0(n^2)
+
+
+
+// Selection sort 
+[1 , 2, 3, 5, 4 ]
+_______ _______
+sorted  unsorted so we do n - 1 iterations to push n - 1 smallest elements 
+here we will push the smallest element to zeroth index then again n - 1 iterations upto for unsorted part ! 
+for(int i = 0 ; i < n ; i++) {
+    let smallestInddex = i ;
+    for(int j = i + 1 ; j < n ; j++) {
+        if(arr[j] < arr[smallestIndex]){
+            smallestIndex = j ;
+        }
+        swap(arr[i] , arr[smallIndex])
+    }
+}
--- LectureNotes/sorting/bubble.sort.cpp
+++ LectureNotes/sorting/bubble.sort.cpp
@@ -2,6 +2,11 @@
 using namespace std ; 
 
 void bubbleSort(int arr[] , int n) {
+
+    // checking 
+
+    bool isSwap = false ;
+
     for(int i = 0 ; i < n - 1 ; i++) {
         for(int j = 0 ; j < n - i - 1 ; j++) {
             if(arr[j] > arr[j + 1]) {
@@ -9,14 +14,17 @@ void bubbleSort(int arr[] , int n) {
                 int temp = arr[j] ; // instead of using this we can use swap built in function
                 arr[j] = arr[j + 1 ] ;
                 arr[j + 1 ] = temp ;
+                isSwap = true ; 
             }
         }
-
+        if(!isSwap ) {
+           return ; 
+        }
     }
 }
 
 int main() {
-    int arr[] = {5 , 3, 8 , 4 , 2};
+    int arr[] = {6 , 8 , 5 , 4};
     int n = sizeof(arr) / sizeof(int) ; 
     bubbleSort(arr , n) ; 
     for(int i = 0 ; i < n ; i++) {
